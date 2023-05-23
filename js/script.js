e7e2324a0b175fc060b04884a4333c91a4428a6e
DELORD ROYAL <royalight24@gmail.com>
2023-05-23 08:32:15 -0700

    implemented number of guest feature

--- js/script.js
+++ js/script.js
@@ -1,5 +1,55 @@
-const reaveal = document.querySelector("#reaveal")
+const hotelSelect = document.querySelector("#hotelSelect")
+const hotelSelectP = hotelSelect.querySelector('p')
+const minusBtn = document.querySelector("#minus"),
+      addBtn = document.querySelector("#add"),
+      amountOfGuest = document.querySelector("#amountOfGuest")
 
-reaveal.addEventListener("click", () => {
+const room = {
+  type: "",
+  amount: ""
+}
+
+hotelSelect.addEventListener("click", () => {
   document.querySelector("#hotelType").classList.toggle('active')
+  document.querySelector("#reveal").classList.toggle('open')
+})
+
+const options = document.querySelectorAll('.option')
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    const roomType = option.dataset.type
+    const roomPrice = option.dataset.price
+    
+    hotelSelectP.innerHTML = roomType + " (NGN " + roomPrice + ")";
+    
+    room.type = roomType
+    room.price = roomPrice
+    
+    document.querySelector("#reveal").classList.toggle('open')
+    document.querySelector("#hotelType").classList.toggle('active')
+  })
+})
+
+minusBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  const value = Number(amountOfGuest.value);
+
+  if(value === 0) {
+    return;
+  }
+
+  amountOfGuest.value --;
+})
+
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  const value = Number(amountOfGuest.value);
+
+  if(value === 10) {
+    return;
+  }
+
+  amountOfGuest.value ++;
 })
